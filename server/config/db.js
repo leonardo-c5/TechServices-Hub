24353075f28c34a271cdd5a0a53a8440542b0fa5
Yassi Araujo <yassifernando8@gmail.com>
2026-04-21 10:02:02 -0500

    Conexión a base de datos en Railway

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,11 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const path = require('path');
+
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
@@ -7,6 +13,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS, 
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql'
   }
 );
