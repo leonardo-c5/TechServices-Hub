0412ef8e62d380b9f089d72b59c3ae4e34d4fbc3
leonardo <tu@ejemplo.com>
2026-04-27 11:52:36 -0500

    Corrigiendo conexion y SSL

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,22 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
-
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_USER:', process.env.DB_USER);
-
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASS, 
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql'
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    }
 );
 
 module.exports = sequelize;
